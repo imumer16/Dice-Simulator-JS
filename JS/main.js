6b08371f31d78662f4270b43d97961728f9f9766
Umer Ahmed <84404257+imumer16@users.noreply.github.com>
2023-03-02 14:39:35 +0500

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,14 @@
+window.onload = function () {
+  var cube = document.getElementById("cube");
+  var min = 1;
+  var max = 24;
+  cube.onclick = function () {
+    var xRand = getRandom(max, min);
+    var yRand = getRandom(max, min);
+    cube.style.transform =
+      "rotateX(" + xRand + "deg) rotateY(" + yRand + "deg)";
+  };
+  function getRandom(max, min) {
+    return (Math.floor(Math.random() * (max - min)) + min) * 90;
+  }
+};
